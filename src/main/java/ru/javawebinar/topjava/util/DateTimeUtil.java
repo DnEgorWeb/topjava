609aff2d523faa56e8ed3cf5b4a4e3a593bf4dab
Egor <egor19942012@gmail.com>
2023-06-12 20:14:16 +0700

    Refactor "isBetweenHalfOpen()": use single generic method instead method overload

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -8,12 +8,8 @@ import java.time.format.DateTimeFormatter;
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
-    }
-
-    public static boolean isBetweenHalfOpen(LocalDateTime ldt, LocalDateTime startTime, LocalDateTime endTime) {
-        return ldt.compareTo(startTime) >= 0 && ldt.compareTo(endTime) < 0;
+    public static <T extends Comparable<T>> boolean isBetweenHalfOpen(T origin, T start, T end) {
+        return origin.compareTo(start) >= 0 && origin.compareTo(end) < 0;
     }
 
     public static LocalDate parseLocalDate(String str, LocalDate fallbackDate) {
