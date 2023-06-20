59e3ea31ee83676a42e9a8cd3f423ca0cf11a163
Egor <egor19942012@gmail.com>
2023-06-20 20:13:32 +0700

    Populate meals in the populateDB.sql

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,7 @@
-DELETE FROM user_role;
-DELETE FROM users;
+DELETE
+FROM user_role;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +12,10 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, datetime, description, calories)
+VALUES (100000, '2023-06-19 10:00:00.000000', 'Завтрак', 500),
+       (100000, '2023-06-19 13:00:00.000000', 'Ланч', 1500),
+       (100000, '2023-06-19 19:00:00.000000', 'Ужин', 800),
+       (100000, '2023-06-20 11:00:00.000000', 'Завтрак', 800),
+       (100000, '2023-06-20 15:00:00.000000', 'Полдник', 300);
