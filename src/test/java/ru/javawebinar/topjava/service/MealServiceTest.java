140ad99a837ffeda3d0935c5575e5f1ae3193116
Egor <egor19942012@gmail.com>
2023-06-27 17:53:16 +0700

    Refactor benchmark of MealServiceTest
    Remove junit-benchmarks library; use built-in TestWatcher class

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,10 +1,13 @@
 package ru.javawebinar.topjava.service;
 
-import com.carrotsearch.junitbenchmarks.BenchmarkRule;
+import org.junit.AfterClass;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,25 +18,48 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
-})
+@ContextConfiguration({"classpath:spring/spring-app.xml", "classpath:spring/spring-db.xml"})
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
+    private static final Logger log = LoggerFactory.getLogger(MealServiceTest.class);
+    private static final List<String> timelog = new ArrayList<>();
+
+    @Rule
+    public final TestWatcher benchmarkRun = new TestWatcher() {
+        LocalDateTime startDateTime;
+
+        @Override
+        protected void starting(Description description) {
+            startDateTime = LocalDateTime.now();
+        }
+
+        @Override
+        protected void finished(Description description) {
+            String testLog = description.getMethodName() + " - " +
+                    ChronoUnit.MILLIS.between(startDateTime, LocalDateTime.now()) + "ms";
+            timelog.add(testLog);
+            log.info(testLog);
+        }
+    };
     @Autowired
     private MealService service;
 
-    @Rule
-    public final TestRule benchmarkRun = new BenchmarkRule();
+    @AfterClass
+    public static void onFinish() {
+        timelog.forEach(log::info);
+    }
 
     @Test
     public void delete() {
@@ -63,8 +89,8 @@ public class MealServiceTest {
 
     @Test
     public void duplicateDateTimeCreate() {
-        assertThrows(DataAccessException.class, () ->
-                service.create(new Meal(null, meal1.getDateTime(), "duplicate", 100), USER_ID));
+        assertThrows(DataAccessException.class,
+                () -> service.create(new Meal(null, meal1.getDateTime(), "duplicate", 100), USER_ID));
     }
 
     @Test
@@ -103,10 +129,8 @@ public class MealServiceTest {
 
     @Test
     public void getBetweenInclusive() {
-        MEAL_MATCHER.assertMatch(service.getBetweenInclusive(
-                        LocalDate.of(2020, Month.JANUARY, 30),
-                        LocalDate.of(2020, Month.JANUARY, 30), USER_ID),
-                meal3, meal2, meal1);
+        MEAL_MATCHER.assertMatch(service.getBetweenInclusive(LocalDate.of(2020, Month.JANUARY, 30),
+                LocalDate.of(2020, Month.JANUARY, 30), USER_ID), meal3, meal2, meal1);
     }
 
     @Test
