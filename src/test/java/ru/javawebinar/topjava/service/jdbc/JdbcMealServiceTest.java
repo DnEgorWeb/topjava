dd64efc4ce49657b2e57e4944ec76077d26a4efd
Egor <egor19942012@gmail.com>
2023-07-16 17:29:20 +0700

    Add transactions to JDBC implementations

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -6,6 +6,5 @@ import ru.javawebinar.topjava.service.AbstractMealServiceTest;
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
-//@Ignore
 public class JdbcMealServiceTest extends AbstractMealServiceTest {
 }
