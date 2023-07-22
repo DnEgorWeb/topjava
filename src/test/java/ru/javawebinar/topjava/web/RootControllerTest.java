2e48e902769ee0f7face4bf3bfd63a8892e61edf
Egor <egor19942012@gmail.com>
2023-07-22 13:04:38 +0700

    Add RootControllerTest.getMeals test

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,31 +2,50 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.ResultActions;
 import ru.javawebinar.topjava.model.User;
 
 import java.util.List;
 
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.*;
 
 class RootControllerTest extends AbstractControllerTest {
 
     @Test
     void getUsers() throws Exception {
-        perform(get("/users"))
-                .andDo(print())
+        perform(get("/users")).andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("users"))
                 .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
-                .andExpect(model().attribute("users",
-                        new AssertionMatcher<List<User>>() {
-                            @Override
-                            public void assertion(List<User> actual) throws AssertionError {
-                                USER_MATCHER.assertMatch(actual, admin, guest, user);
-                            }
-                        }
-                ));
+                .andExpect(model().attribute("users", new AssertionMatcher<List<User>>() {
+                    @Override
+                    public void assertion(List<User> actual) throws AssertionError {
+                        USER_MATCHER.assertMatch(actual, admin, guest, user);
+                    }
+                }));
+    }
+
+    @Test
+    void getMeals() throws Exception {
+        ResultActions resultActions = perform(get("/meals")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(meals.size())));
+        meals.forEach(meal -> {
+            try {
+                resultActions.andExpect(model().attribute("meals", hasItem(allOf(hasProperty("id", is(meal.getId())),
+                        hasProperty("dateTime", is(meal.getDateTime())),
+                        hasProperty("description", is(meal.getDescription())),
+                        hasProperty("calories", is(meal.getCalories()))))));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
